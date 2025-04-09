fc5c465dd42dc2509b67641e357b42e206dec309
Rafael Manasyan <rafaelmanasyan@Rafaels-MacBook-Air.local>
2025-04-09 16:20:08 +0400

    MenuItem added

---
+++ src/MenuItem.java
@@ -0,0 +1,29 @@
+public class MenuItem {
+    private int id;
+    private String name;
+    private double price;
+    private String category;
+
+    public MenuItem(int id, String name, double price, String category) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.category = category;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+}
